45d4beaf67c697f4245ba9bfe8832094f67a102b
Gulshanjangid <gulshanjangid333@gmail.com>
2026-09-17 12:13:32 +0530

    swap done

---
+++ Swap.py
@@ -0,0 +1,7 @@
+a = 5
+b = 10
+print("Before swap: a =", a, ", b =", b)
+temp = a   
+a = b
+b = temp
+print("After swap:  a =", a, ", b =", b)
---
+++ swap2.py
@@ -0,0 +1,6 @@
+a = 5
+b = 10
+print("Before swap: a =", a, ", b =", b)
+
+a ,b = b,a
+print("After swap:  a =", a, ", b =", b)
