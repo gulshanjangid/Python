fd2e71f13403b0f0fbd3e97591e706f985baeab5
Gulshanjangid <gulshanjangid333@gmail.com>
2026-09-21 15:18:07 +0530

    done

--- OOPS/Student.py
+++ OOPS/Student.py
@@ -1,12 +1,21 @@
 class Student:
 
-    def __init__(self, name):
+    # name = "Gulshan"
+    # age = 25
+
+
+    def __init__(self):
+        print("Student object created")
+
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
     def show(self):
         print(self.name)
 
 
-S1 = Student("Gulshan") 
-S1.show() 
-
+student1 = Student("gulshan",22)
+student1.show()
+print(student1.name)
+print(student1.age)
