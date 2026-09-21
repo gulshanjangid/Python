efebf275bd64aea7c7576769fcd39de1152b7dcf
Gulshanjangid <gulshanjangid333@gmail.com>
2026-09-21 11:55:47 +0530

    operators done

--- operators.py
+++ operators.py
@@ -1,8 +1,32 @@
-a = 10
-b = 3
+a = 20
+b = 5
 
-print(a + b)   
-print(a - b)   
-print(a * b)   
-print(a / b)   
-print(a // b)  # Floor division
+# Arithmetic
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
+
+# Comparison
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater:", a > b)
+print("Less:", a < b)
+
+# Logical
+print("AND:", a > 10 and b < 10)
+print("OR:", a < 10 or b < 10)
+print("NOT:", not(a > b))
+
+# Membership
+numbers = [10, 20, 30]
+
+print("20 in list:", 20 in numbers)
+print("50 not in list:", 50 not in numbers)
+
+# Identity
+x = numbers
+print("Same object:", x is numbers)
