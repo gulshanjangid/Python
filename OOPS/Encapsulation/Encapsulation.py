4220f9b010ab003d60cdcfbccc040ddc9772710a
Gulshanjangid <gulshanjangid333@gmail.com>
2026-09-24 12:42:26 +0530

    done

---
+++ OOPS/Encapsulation/Encapsulation.py
@@ -0,0 +1,8 @@
+#Encapsulation means keeping data and the methods that work on that data together inside a class, and controlling how that data can be accessed or changed.
+
+# Think of it like an ATM:
+
+# Your bank balance is internal data.
+# You don't directly change the balance.
+# You use methods like deposit() and withdraw().
+# The methods control what is allowed.
